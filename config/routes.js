83fbbb7ee8da8a4f581bb4c395b0b5d2b06cfb9e
matt0418 <mfeldma7@gmail.com>
2019-03-08 11:21:32 -0500

    added ability to register new user

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,10 @@
 const axios = require('axios');
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const Users = require('./helper')
 
-const { authenticate } = require('../auth/authenticate');
+
+const { authenticate, jwtKey } = require('../auth/authenticate');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -8,8 +12,17 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
-function register(req, res) {
-  // implement user registration
+async function register(req, res) {
+  let user = req.body
+  const hash = bcrypt.hashSync(user.password, 12)
+  user.password = hash
+  try {
+    const newUser = await Users.add(user)
+    res.status(201).json(newUser)
+  } catch(error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
 }
 
 function login(req, res) {
@@ -29,4 +42,15 @@ function getJokes(req, res) {
     .catch(err => {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  }
+  const options = {
+    expiresIn: '1d'
+  }
+  return jwt.sign(payload, jwtKey, options)
 }
